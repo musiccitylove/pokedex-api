73a83bc83dc0496d3fdcf8dc553a312709cc4107
Charles <charlesdpulliam@gmail.com>
2020-02-21 14:33:34 -0600

    something is brokemon

--- server.js
+++ server.js
@@ -1,13 +1,39 @@
+require('dotenv').config()
 const express = require('express')
 const morgan = require('morgan')
+
+console.log(process.env.API_TOKEN)
 
 const app = express()
 
 app.use(morgan('dev'))
 
-app.use((req, res) => {
-  res.send('Hello, worldly people!')
-})
+app.use(function validateBearerToken(req, res, next) {
+  const bearerToken = req.get('Authorization').split(' ')[1]
+  const apiToken = process.env.API_TOKEN
+
+    console.log('validate bearer token middleware')
+    
+  if (bearerToken !== apiToken) {
+      return res.status(401).json({ error: 'Unauthorized request' })
+    } 
+   // move to the next middleware
+   next()
+ })
+
+const validTypes = [`Bug`, `Dark`, `Dragon`, `Electric`, `Fairy`, `Fighting`, `Fire`, `Flying`, `Ghost`, `Grass`, `Ground`, `Ice`, `Normal`, `Poison`, `Psychic`, `Rock`, `Steel`, `Water`]
+
+function handleGetTypes(req, res) {
+    res.json(validTypes)
+   }
+  
+   app.get('/types', handleGetTypes)
+
+function handleGetPokemon(req, res) {
+    res.send('Hello, Pokemon!')
+   }
+    
+   app.get('/pokemon', handleGetPokemon)
 
 const PORT = 8000
 
